e5ede29ad6ed1937008b54a477f4b2cff2ae39e6
VistaBuda <yingwaness@gmail.com>
2022-10-04 07:39:29 +0200

    Test runner fix

--- src/test/java/com/automatedtest/vodacom/TestRunner.java
+++ src/test/java/com/automatedtest/vodacom/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/com/automatedtest/oldmutual/Assessment.feature"},
+@CucumberOptions(features = {"src/test/resources/com/automatedtest/vodacom/Assessment.feature"},
         strict = false, plugin = {"pretty",
         "json:target/cucumber_json_reports/home-page.json",
         "html:target/home-page-html"},
